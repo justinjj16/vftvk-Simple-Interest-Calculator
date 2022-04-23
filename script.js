91ab6057663b993486675a888747a5f53cef04c1
justin <justin.j@neoito.com>
2022-04-23 14:35:15 -0500

    new Changes Working Condition

--- script.js
+++ script.js
@@ -1,6 +1,35 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
-}
-        
+const compute = () => {
+  const elementsValues = getElementsValues();
+  let element = document.getElementById("result-container");
+  element.classList.remove("hidden");
+  const { principal, rate, years } = elementsValues;
+  showDisplay(principal, rate, years);
+};
+
+const updateTextInput = (val) => {
+  document.getElementById("output").value = val;
+  const elementsValues = getElementsValues();
+  const { principal, rate, years } = elementsValues;
+  showDisplay(principal, rate, years);
+};
+const handleEvent = () => {
+  const elementsValues = getElementsValues();
+  const { principal, rate, years } = elementsValues;
+  showDisplay(principal, rate, years);
+};
+
+const getElementsValues = () => {
+  let principal = document.getElementById("principal").value;
+  let rate = document.getElementById("rate").value;
+  let years = document.getElementById("years").value;
+  return { principal, rate, years };
+};
+
+const showDisplay = (principal, rate, years) => {
+  document.getElementById("principalOutput").innerHTML = principal;
+  document.getElementById("rateOutput").innerHTML = rate;
+  const getYear = new Date().getFullYear();
+  document.getElementById("yearsOutput").innerHTML = getYear + parseInt(years);
+  let result = (principal * rate * years) / 100;
+  document.getElementById("result").innerHTML = result;
+};
